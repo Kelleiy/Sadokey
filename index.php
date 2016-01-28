fcf7cd8bbb583f721ce3cf6876d770c941c3726a
unknown <148445@WKS-HP-1746.ASG.LOCAL>
2016-01-28 12:18:23 +0100

    index en puzzle aangepast, foutmelding verholpen, functions 'opgeroepen'

--- index.php
+++ index.php
@@ -5,17 +5,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo ";;";
+echo "alive";
 include "puzzle.php";
 
 naarTabel($word_list);
-createPuzzle($puzzle_level, $word_list);
-createPuzzleTable($data);
-fillRandomLetters($data);
-prepareData();
-getRandomLetter();
-getRandomStartPosition($word, $level);
-hasOverlap($data, $word, $position);
-losLevelOp();
+$s = createPuzzle($puzzle_level, $word_list);
+echo $s;
+//createPuzzleTable($data);
+//getRandomLetter();
+//getRandomStartPosition($word, $level);
+//hasOverlap($data, $word, $position);
+//losLevelOp();
 
 ?>
