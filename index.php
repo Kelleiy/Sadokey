7a56ff90534d3e415b0f4fc4f4021c198b14566c
Samantha Heymans <148445@hpleerling.nl>
2016-02-01 15:48:05 +0100

    php (oproepen) in html aangepast

--- index.php
+++ index.php
@@ -45,8 +45,19 @@
                 <tr>
                     <td>
                         <?php
-                            echo createPuzzle($puzzle_level, $word_list);
-                        ?>
+echo "alive";
+include "puzzle.php";
+
+naarTabel($word_list);
+$s = createPuzzle($puzzle_level, $word_list);
+echo $s;
+//createPuzzleTable($data);
+//getRandomLetter();
+//getRandomStartPosition($word, $level);
+//hasOverlap($data, $word, $position);
+//losLevelOp();
+
+?>
                     </td>
                 </tr>
                 <tr>
