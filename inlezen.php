ee40a140cd077d8a1cd9c1f8abbaa3a7d0e74378
Samantha Heymans <148445@hpleerling.nl>
2016-02-10 13:31:59 +0100

    Hoofdletters terug veranderd

--- inlezen.php
+++ inlezen.php
@@ -20,8 +20,8 @@
 //include 'losoplevel1.php';
 //losoplevel1 ($wz, $woorden);
 
-function inlezen($filename ) {
-    $ruweregels = FILE($filename);
+function inlezen($fileName ) {
+    $ruweRegels = FILE($filename);
 }
     foreach($ruweregels as $regel) {
         $regel = trim($regel);
